dfd950d3feecbaae3546259a1d30f2a444c1f485
pavel.shvydkyi <pavel.shvydkyi@swansoftwaresolutions.com>
2022-06-09 18:09:41 +0300

    Aading repository interfaces

---
+++ PP/TierCore/Repository/IOrderRepositoty.cs
@@ -0,0 +1,9 @@
+﻿namespace PP.TierCore.Repository
+{
+    public interface IOrderRepositoty<TEntity, TResouce> : IRepository<TEntity, TResouce>
+        where TResouce : class
+        where TEntity : class
+    {
+
+    }
+}
